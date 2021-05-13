9e0f9552fce1dd234a8c5c19cfb7cfa87230ed2b
mongbro <whd4607@naver.com>
2021-05-13 15:48:02 +0900

    second

--- lib/connect_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$dbpasswd = "";
-	$connect = mysqli_connect("localhost", "root") or die("mysql���� ���� ����");
-	$db = mysqli_select_db($connect, 'oswtf');
-	mysqli_select_db($connect, 'oswtf') or die("DB ���� ����");
-?>
